9f2dcdcb56d51b3ff8d9404726285a14662332ed
Denis Eroshevskii <devskjj@gmail.com>
2026-01-08 16:08:05 +0600

    "Сделал get /api/users/{userId}/statistics."

--- src/main/java/kg/attractor/labwork_55/dao/RatingDao.java
+++ src/main/java/kg/attractor/labwork_55/dao/RatingDao.java
@@ -12,7 +12,6 @@ import java.util.List;
 public class RatingDao {
     private final JdbcTemplate jdbcTemplate;
 
-
     public List<UserRatingDto> getTopTenRatings() {
         String sql = "SELECT u.username, u.email, t.score FROM users u JOIN quiz_results qr ON u.id = qr.user_id JOIN top_scores t ON t.result_id = qr.id ORDER BY t.score DESC LIMIT 10";
 
--- src/main/java/kg/attractor/labwork_55/services/impl/UserServiceImpl.java
+++ src/main/java/kg/attractor/labwork_55/services/impl/UserServiceImpl.java
@@ -1,17 +1,22 @@
 package kg.attractor.labwork_55.services.impl;
 
+import kg.attractor.labwork_55.dao.UserStatisticsDao;
 import kg.attractor.labwork_55.dto.CreateUserDto;
 import kg.attractor.labwork_55.dto.UserStatisticsDto;
+import kg.attractor.labwork_55.exceptions.UserNotFoundException;
 import kg.attractor.labwork_55.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserStatisticsDao userStatisticsDao;
 
     @Override
     public Integer registerUser(CreateUserDto dto, Authentication auth) {
@@ -20,6 +25,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserStatisticsDto getStatistics(Integer userId, Authentication auth) {
-        return null;
+        if (!userStatisticsDao.userExists(userId)) {
+            throw new UserNotFoundException("User with id " + userId + " not found");
+        }
+
+        log.info("Getting statistics for user {}", userId);
+
+        Map<String, Object> statistics = userStatisticsDao.getStatistics(userId);
+        return UserStatisticsDto.builder()
+                .statistics(statistics)
+                .build();
     }
 }
